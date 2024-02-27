1c50f2ded885c422447c40b92182e9cc35949978
johnspangenberg <dev@springbok.space>
2024-02-27 13:06:08 +0100

    Removed credentials

---
+++ src/main/java/space/springbok/bslimageservice/controller/ImageController.java
@@ -0,0 +1,13 @@
+package space.springbok.bslimageservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class ImageController {
+
+    public static final String IMAGE_
+}
